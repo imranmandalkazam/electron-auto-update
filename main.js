dc1b87f58d9f242d22cc0054917923e7b28b4160
imranmandalkazam <imran@kazam.in>
2022-07-29 15:23:54 +0530

    Update 1.0.2 released

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ function createWindow() {
   mainWindow.on("closed", function () {
     mainWindow = null;
   });
-
   mainWindow.once("ready-to-show", () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
@@ -44,6 +43,7 @@ ipcMain.on("app_version", (event) => {
 autoUpdater.on("update-available", () => {
   mainWindow.webContents.send("update_available");
 });
+
 autoUpdater.on("update-downloaded", () => {
   mainWindow.webContents.send("update_downloaded");
 });
